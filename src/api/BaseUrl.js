74d4046a11c171742abe5678a8ffbef7c053c4ad
javohir <javoxir8177@gmail.com>
2025-09-11 11:44:35 +0500

    Bace url

--- src/api/BaseUrl.js
+++ src/api/BaseUrl.js
@@ -1,2 +1,2 @@
-// export const BASE_URL = "https://shop-bot.javohir-dev.uz/api/v1";
-export const BASE_URL = "http://localhost:5050/api/v1";
+export const BASE_URL = "https://ming-chinor.javohir-dev.uz/api/v1";
+// export const BASE_URL = "http://localhost:5050/api/v1";
